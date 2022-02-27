da292b1cb4a4a77c32a65e19427f374f4f4b1764
rossog93 <rosso.gabriel1@gmail.com>
2022-02-27 13:56:12 -0300

    Read ds18b20 from fw.

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,15 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <LiquidCrystal_I2C.h>
-
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define DHTTYPE DHT11   // DHT 11
 #define DHTPIN 15 
+#define ONE_WIRE_BUS 13
 
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -25,19 +29,35 @@ void setup()
 
   //start DHT
   dht.begin();
+
+  //start ds18b20
+  sensors.begin();
 }
 
+void read_18b20(void)
+{
+    sensors.requestTemperatures(); 
+    delay(5000);
+    float temperatureC = sensors.getTempCByIndex(0);
 
+    Serial.print("ds18B20 sensor ");
+    Serial.print(temperatureC);
+    Serial.println("ºC");
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("[18B20 sensor]");
+    lcd.setCursor(0,1);
+    lcd.print("Temp:");
+    lcd.print(temperatureC);
+    lcd.print("C");
+}
 
 void read_dht(void)
 {
    // Wait a few seconds between measurements.
   delay(2000);
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
 
   // Check if any reads failed and exit early (to try again).
@@ -60,18 +80,22 @@ void read_dht(void)
   Serial.print(hic);
   Serial.print(F("°C \n"));
 
+  lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Tmp: ");
+  lcd.print("Tmp:");
   lcd.print(t);
-  lcd.print(" C");
+  lcd.print("C [DHT11]");
   lcd.setCursor(0,1);
-  lcd.print("Hum: ");
+  lcd.print("Hum:");
   lcd.print(h);
   lcd.print(" %HR");
 }
 
+
+
 void loop()
 {
 read_dht();
+read_18b20();
 }
 
