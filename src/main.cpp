d5581346e4ca9054a789882c83c10101aa8e0df6
rossog93 <rosso.gabriel1@gmail.com>
2022-02-27 11:10:18 -0300

    Add file structure and library for I2C 16x2 LCD display

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * Displays text sent over the serial port (e.g. from the Serial Monitor) on
+ * an attached LCD.
+ * YWROBOT
+ *Compatible with the Arduino IDE 1.0
+ *Library version:1.1
+ */
+#include <Arduino.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();
+  lcd.clear();
+  Serial.begin(9600);
+  Serial.println("Start!");
+}
+
+void loop()
+{
+lcd.setCursor(0,0);
+lcd.print("Hola como estas");
+lcd.setCursor(0,1);
+lcd.print("Second line...");
+}
