de174c297df7e7975e35e4bbdedf43e02025a9b7
rossog93 <rosso.gabriel1@gmail.com>
2022-02-27 13:08:20 -0300

    Add DHT sensors reading.

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,77 @@
-/*
- * Displays text sent over the serial port (e.g. from the Serial Monitor) on
- * an attached LCD.
- * YWROBOT
- *Compatible with the Arduino IDE 1.0
- *Library version:1.1
- */
 #include <Arduino.h>
 #include <Wire.h> 
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 #include <LiquidCrystal_I2C.h>
 
+
+#define DHTTYPE DHT11   // DHT 11
+#define DHTPIN 15 
+
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
-  lcd.init();                      // initialize the lcd 
+
+  //start lcd
+  lcd.init();
   lcd.backlight();
   lcd.clear();
+
+  //Start Serial
   Serial.begin(9600);
   Serial.println("Start!");
+
+  //start DHT
+  dht.begin();
+}
+
+
+
+void read_dht(void)
+{
+   // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) ) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.printf("Read DHT sensor:\n");
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(F(" Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C \n"));
+
+  lcd.setCursor(0,0);
+  lcd.print("Tmp: ");
+  lcd.print(t);
+  lcd.print(" C");
+  lcd.setCursor(0,1);
+  lcd.print("Hum: ");
+  lcd.print(h);
+  lcd.print(" %HR");
 }
 
 void loop()
 {
-lcd.setCursor(0,0);
-lcd.print("Hola como estas");
-lcd.setCursor(0,1);
-lcd.print("Second line...");
+read_dht();
 }
+
